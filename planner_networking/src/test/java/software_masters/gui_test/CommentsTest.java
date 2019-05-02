bf207cbdf3ea8ce24f57eb0e186b70efae5c482b
mhkend01 <lee.kendall@centre.edu>
2019-05-02 18:03:37 -0400

    Finished task two test

--- planner_networking/src/test/java/software_masters/gui_test/CommentsTest.java
+++ planner_networking/src/test/java/software_masters/gui_test/CommentsTest.java
@@ -6,9 +6,15 @@ import static org.testfx.api.FxAssert.verifyThat;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeView;
 import software_masters.planner_networking.Node;
+import javafx.scene.input.KeyCode;
+
+import org.testfx.api.FxAssert;
+import org.testfx.matcher.control.ListViewMatchers;
 
 class CommentsTest extends GuiTestBase {
 	
@@ -23,6 +29,7 @@ class CommentsTest extends GuiTestBase {
 		getToPlanEditView();
 		defaultTest();
 		testAddComment();
+		testAlternativeUser();
 		testRemoveComment();
 		
 	}
@@ -47,15 +54,17 @@ class CommentsTest extends GuiTestBase {
 		verify("#removeCommentButton","Remove Comment");
 	}
 	
+	/**
+	 * Tests that users can add comments
+	 */
 	private void testAddComment()
 	{
 		clickOn("#commentField");
 		write("I have an idea");
 		clickOn("#submitCommentButton");
-		sleep(3000);
 		
 		//will fail if comment not added to list view, with the username concatenated to the comment
-		Assert.assertTrue("#commentListView".contains("User: I have an idea"));
+		 FxAssert.verifyThat("#commentListView", ListViewMatchers.hasListCell("user: I have an idea"));
 		
 		//Verifies "isPushed" boolean is changed after commenting - the save warning popup should be displayed
 		//if adding a comment without saving.
@@ -66,16 +75,92 @@ class CommentsTest extends GuiTestBase {
 		//Verifies list view of comments changes when the user changes sections
 		doubleClickOn("Mission");
 		clickOn("Goal");
-		Assert.assertFalse("#commentListView".contains("User: I have an idea"));
+		FxAssert.verifyThat("#commentListView", ListViewMatchers.isEmpty());
+		clickOn("#commentField");
+		write("We need a goal");
+		clickOn("#submitCommentButton");
+		FxAssert.verifyThat("#commentListView", ListViewMatchers.hasListCell("user: We need a goal"));
 		
+		//Verifies comment reappears when user navigates back to correct section
+		clickOn("Mission");
+		FxAssert.verifyThat("#commentListView", ListViewMatchers.hasListCell("user: I have an idea"));
+		clickOn("Goal");
+		FxAssert.verifyThat("#commentListView", ListViewMatchers.hasListCell("user: We need a goal"));
+	}
+	
+	/**
+	 * Tests that comments can be seen by users other than the one who left the comment
+	 */
+	private void testAlternativeUser()
+	{
 		clickOn("#logoutButton");
 		clickOn("Yes");
+		clickOn("#usernameField");
+		for(int i = 0; i<4; i++)
+		{
+			press(KeyCode.BACK_SPACE);
+			release(new KeyCode[] {});	
+		}
+		
+		write("admin");
+		
+		clickOn("#passwordField");
+		for(int i = 0; i<4; i++)
+		{
+			press(KeyCode.BACK_SPACE);
+			release(new KeyCode[] {});	
+		}
+		write("admin");
+		
+		clickOn("#loginButton");
+		clickOn("2019");
+		//Verifies users can see comments left by other users
+		FxAssert.verifyThat("#commentListView", ListViewMatchers.hasListCell("user: I have an idea"));
+		doubleClickOn("Mission");
+		clickOn("Goal");
+		FxAssert.verifyThat("#commentListView", ListViewMatchers.hasListCell("user: We need a goal"));
 		
 	}
 	
+	/**
+	 * Tests that users can remove comments
+	 */
 	private void testRemoveComment()
-	{}
-	
+	{
+		//verifies comment is removed from listview
+		clickOn("Mission");
+		clickOn("user: I have an idea");
+		clickOn("Remove Comment");
+		FxAssert.verifyThat("#commentListView", ListViewMatchers.isEmpty());
+		
+		//verifies the warning popup is diaplayed in response
+		// to comment deletions, and that the comment is now absent when another user views the same section
+		clickOn("#logoutButton");
+		checkPopupMsg("You have unsaved changes. Do you wish to save before exiting?");
+		clickOn("Yes");
+		clickOn("#loginButton");
+		clickOn("2019");
+		FxAssert.verifyThat("#commentListView", ListViewMatchers.isEmpty());
+		
+		clickOn("#commentField");
+		write("I have another idea");
+		clickOn("#submitCommentButton");
+		
+		clickOn("#commentField");
+		write("So many ideas");
+		clickOn("#submitCommentButton");
+		
+		clickOn("user: So many ideas");
+		clickOn("Remove Comment");
+		doubleClickOn("Mission");
+		clickOn("Goal");
+		clickOn("Mission");
+		
+		//Verifies only the selected comment is removed correctly, by confirming the listview only has one
+		//comment, the comment not intended for deletion.
+		FxAssert.verifyThat("#commentListView", ListViewMatchers.hasListCell("user: I have another idea"));
+		FxAssert.verifyThat("#commentListView", ListViewMatchers.hasItems(1));
+	}
 	
 	
 
